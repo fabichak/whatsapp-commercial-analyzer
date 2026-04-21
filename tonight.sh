cc693c72288f5c894210dfada6bfdcb878493ab9
Martin Fabichak <mfabichak@gmail.com>
2026-04-21 10:13:43 -0300

    M2-S3-T1

--- tonight.sh
+++ tonight.sh
@@ -8,8 +8,6 @@ sleep 2m
 claude --dangerously-skip-permissions -p "Speak ike a caveman. Using Context7, sequential-thinking. Read @TECH_PLAN.md and then work on M2-S4-T1 and verify the it according to spec. Do not read PROGRESS.md."
 git add .
 git commit -m "M2-S4-T1"
-
-sleep 2m
 claude --dangerously-skip-permissions -p "Speak ike a caveman. Using Context7, sequential-thinking. Read @TECH_PLAN.md and then work on M2-S4-T2 and verify the it according to spec. Do not read PROGRESS.md."
 git add .
 git commit -m "M2-S4-T2"
